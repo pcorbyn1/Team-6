2214315aa43d71c6e4735e0c4fbb71998ae0f541
Frannie Stewart <franniestewart@Frannies-MacBook-Air.local>
2022-11-19 15:43:35 -0400

    added doctor.java file

---
+++ Doctor.java
@@ -0,0 +1,24 @@
+public class Doctor {
+    private final int DOCTOR_ID;
+    private String name;
+    private String specialty;
+
+    public Doctor(int doctorIn, String nameIn, String specialtyIn){
+        DOCTOR_ID = doctorIn;
+        name = nameIn;
+        specialty = specialtyIn;
+    }
+
+    public int getDoctor() {
+        return DOCTOR_ID;
+    }
+
+    public String getDoctorName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+}
